27281c5fad3910e80053ffe9c1cdaf0334c4ed02
EouzanRaphaelEtudiantAccount <134294191+EouzanRaphaelEtudiantAccount@users.noreply.github.com>
2025-04-15 11:08:13 +0200

    Feat/tn8 19 (#8)
    * feat: add Winston Logger
    
    * remove Morgan Logger (previous logger)
    
    * update pipeline
    
    to remove before merge
    
    * remove previous update on pipeline
    
    * update README.md + docker compose
    
    * remove comment
    
    * fix docker compose dev
    
    ---------
    
    Co-authored-by: Eouzan Raphael <r.eouzan@akelio.fr>

---
+++ back_student/src/middlewares/loggerMiddelware.ts
@@ -0,0 +1,19 @@
+import {NextFunction, Request, Response} from "express";
+import {winstonLogger} from "../logger";
+export const loggerMiddelware = (req: Request, res: Response, next: NextFunction) => {
+
+    const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+    const start = Date.now();
+
+    winstonLogger.info(`[${new Date().toISOString()}] Started ${req.method} ${req.path} for ${ip}`)
+    winstonLogger.info(`[${new Date().toISOString()}] Started with token`, req.headers['auth'])
+
+    res.on('finish', () => {
+        const duration = Date.now() - start;
+        if (res.statusCode >= 400) {
+            winstonLogger.error("Started with token body ", req.body)
+        }
+        winstonLogger.info(`[${new Date().toISOString()}] Completed ${res.statusCode} in (${duration}ms)`);
+    });
+    next();
+};
